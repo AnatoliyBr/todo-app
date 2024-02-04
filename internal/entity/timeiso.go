9f53b2cc859d061c089b8362dbca37da1a703005
AnatoliyBr <anatoliyb2000@mail.ru>
2024-02-04 19:36:51 +0300

    fix timeiso type

--- internal/entity/timeiso.go
+++ internal/entity/timeiso.go
@@ -2,29 +2,33 @@ package entity
 
 import (
 	"bytes"
+	"fmt"
 	"time"
 )
 
+const (
+	defaultTimeLayout = "2006-01-02T15:04:05"
+)
+
 type TimeISO struct {
-	*time.Time
+	time.Time
 }
 
 func (t *TimeISO) MarshalJSON() ([]byte, error) {
-	return []byte(t.Format(time.DateTime)), nil
+	return []byte(fmt.Sprintf(`"%s"`, t.Time.Format(defaultTimeLayout))), nil
 }
 
-func (t *TimeISO) UnMarshalJSON(data []byte) error {
-
+func (t *TimeISO) UnmarshalJSON(data []byte) error {
 	// from json doc: by convention, unmarshalers implement UnmarshalJSON([]byte("null")) as a no-op
 	if bytes.Equal(data, []byte("null")) {
 		return nil
 	}
 
-	time, err := time.Parse(time.DateTime, string(data))
+	time, err := time.Parse(`"`+defaultTimeLayout+`"`, string(data))
 	if err != nil {
 		return err
 	}
 
-	t = &TimeISO{&time}
+	t.Time = time
 	return nil
 }
